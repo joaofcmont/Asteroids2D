fb10ebe8afe5f06dab2b1086d59f050d242b60c0
João Monteiro <114676364+joaofcmont@users.noreply.github.com>
2025-10-30 15:13:12 +0000

    add asteroids random creation

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
-    Player.containers = (updatables,drawables)
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatables, drawables)
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = updatables
+    asteroid_field = AsteroidField()
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    asteroid_field = AsteroidField()
+
     clock = pygame.time.Clock()
-    dt = 0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x,y)
-    while True:
+    running = True
+
+    while running:
+        dt = clock.tick(60) / 1000  # delta time in seconds
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
+
         screen.fill("black")
-        for drawable in drawables:
-            drawable.draw(screen)
-        pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick(60) / 1000  
+
         updatables.update(dt)
 
+        for drawable in drawables:
+            drawable.draw(screen)
+
+        pygame.display.flip()
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
-
